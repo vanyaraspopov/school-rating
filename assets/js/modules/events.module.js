0ed25697d1665dc78fd7778c20dda623f75db53b
RV <vanyaraspopov@mail.ru>
2017-10-16 13:01:21 +0500

    Добавлен виджет карты, определяющий место по адресу

--- assets/js/modules/events.module.js
+++ assets/js/modules/events.module.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//  Виджет "Показать больше мероприятий"
 (function ($) {
     var eventsListSelector = '#past-events-list';
     var moreEventsBtnSelector = '#more-events-btn';
@@ -44,6 +45,7 @@
     });
 })(jQuery);
 
+//  Виджет "Заявка на участие в меоприятии"
 (function ($) {
     //  Селекторы элементов
     const participationBtnSelector = "#participationBtn";
@@ -81,7 +83,7 @@
     const out_btnText = "Отозвать заявку на участие";
     const out_successMsg = 'Заявка успешно снята';
 
-    var in_successHandler = function (data)  {
+    var in_successHandler = function (data) {
         data = JSON.parse(data);
         if (data.error) {
             $in_modalFormMsg.text(data.errorMsg);
@@ -93,7 +95,7 @@
         }
     };
 
-    var out_successHandler = function (data)  {
+    var out_successHandler = function (data) {
         data = JSON.parse(data);
         if (data.error) {
             $out_modalFormMsg.text(data.errorMsg);
@@ -131,3 +133,66 @@
         return false;
     });
 })(jQuery);
+
+//  Виджет карты
+(function ($, ymaps) {
+    ymaps.ready(init);
+
+    function init() {
+        const mapId = 'map';
+        var $map = $('#' + mapId);
+        var address = $map.data('address');
+
+        var myMap = new ymaps.Map(mapId, {
+            center: [55.753994, 37.622093],
+            zoom: 9
+        });
+
+        // Поиск координат
+        ymaps.geocode(address, {
+            /**
+             * Опции запроса
+             * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/geocode.xml
+             */
+            // Сортировка результатов от центра окна карты.
+            // boundedBy: myMap.getBounds(),
+            // strictBounds: true,
+            // Вместе с опцией boundedBy будет искать строго внутри области, указанной в boundedBy.
+            // Если нужен только один результат, экономим трафик пользователей.
+            results: 1
+        }).then(function (res) {
+            // Выбираем первый результат геокодирования.
+            var firstGeoObject = res.geoObjects.get(0),
+                // Координаты геообъекта.
+                coords = firstGeoObject.geometry.getCoordinates(),
+                // Область видимости геообъекта.
+                bounds = firstGeoObject.properties.get('boundedBy');
+
+            firstGeoObject.options.set('preset', 'islands#darkBlueDotIcon');
+            // Получаем строку с адресом и выводим в иконке геообъекта.
+            firstGeoObject.properties.set('iconCaption', firstGeoObject.getAddressLine());
+
+            // Добавляем первый найденный геообъект на карту.
+            myMap.geoObjects.add(firstGeoObject);
+            // Масштабируем карту на область видимости геообъекта.
+            myMap.setBounds(bounds, {
+                // Проверяем наличие тайлов на данном масштабе.
+                checkZoomRange: true
+            });
+
+            /**
+             * Если нужно добавить по найденным геокодером координатам метку со своими стилями и контентом балуна, создаем новую метку по координатам найденной и добавляем ее на карту вместо найденной.
+             */
+            /**
+             var myPlacemark = new ymaps.Placemark(coords, {
+             iconContent: 'моя метка',
+             balloonContent: 'Содержимое балуна <strong>моей метки</strong>'
+             }, {
+             preset: 'islands#violetStretchyIcon'
+             });
+
+             myMap.geoObjects.add(myPlacemark);
+             */
+        });
+    }
+})(jQuery, ymaps);
