978991ffbb0adc3949012ae3f0e76d52cd58ce0a
RV <vanyaraspopov@mail.ru>
2017-10-30 11:47:13 +0500

    Добавлен скрипт изменения пароля пользователя через ajax

--- assets/js/modules/user-profile.module.js
+++ assets/js/modules/user-profile.module.js
@@ -17,3 +17,43 @@
 
     $filterForm.on('submit', submitFormHandler);
 })(jQuery);
+
+
+//  Изменение пароля
+(function ($) {
+    //  cp = change password
+    var cpFormSelector = '#changePasswordForm';
+    var cpErrorMessageSelector = '#cpErrorMessage';
+    var cpSuccessMessageSelector = '#cpSuccessMessage';
+
+    var $cpForm = $(cpFormSelector);
+    var $cpErrorMessage = $(cpErrorMessageSelector);
+    var $cpSuccessMessage = $(cpSuccessMessageSelector);
+    var ajaxUrl = $cpForm.data('ajax-url');
+
+    $cpForm.on('submit', submitHandler);
+
+    function submitHandler() {
+        var data = $cpForm.serializeArray();
+        $.ajax({
+            url: ajaxUrl,
+            method: 'POST',
+            data: data,
+            success: successHandler
+        });
+        return false;
+    }
+
+    function successHandler(res) {
+        var response = JSON.parse(res);
+        if (response.error) {
+            $cpErrorMessage.text(response.errorMsg);
+            $cpSuccessMessage.hide();
+            $cpErrorMessage.show();
+        } else {
+            $cpErrorMessage.hide();
+            $cpSuccessMessage.show();
+            $cpForm[0].reset();
+        }
+    }
+})(jQuery);
