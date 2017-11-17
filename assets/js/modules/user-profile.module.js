def1ad3521741c4087781ebf576b2914b4b84bad
RV <vanyaraspopov@mail.ru>
2017-11-17 19:48:48 +0500

    Добавлена круговая диаграмма на страницу профиля

--- assets/js/modules/user-profile.module.js
+++ assets/js/modules/user-profile.module.js
@@ -147,3 +147,36 @@ $(function () {
         }
     }
 });
+
+
+//  Круговая диаграмма
+$(document).ready(function () {
+    var chart = 'chart';
+    var $chart = $('#' + chart);
+    var data = $chart.data('rating');
+
+    var chartData = [];
+    for (var name in data) {
+        if (data.hasOwnProperty(name)) {
+            chartData.push([name, data[name]]);
+        }
+    }
+
+    var plot1 = $.jqplot(chart, [chartData], {
+        gridPadding: {top: 0, bottom: 38, left: 0, right: 0},
+        seriesDefaults: {
+            renderer: $.jqplot.PieRenderer,
+            trendline: {show: false},
+            rendererOptions: {padding: 8, showDataLabels: true}
+        },
+        legend: {
+            show: true,
+            placement: 'outside',
+            rendererOptions: {
+                numberRows: 1
+            },
+            location: 's',
+            marginTop: '15px'
+        }
+    });
+});
